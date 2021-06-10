53c2b1004ed57757b248e33519cf101629c84bad
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-10 17:55:43 +0200

    solved 25th exercise

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -86,8 +86,13 @@ suite("Functional Tests with Zombie.js", function () {
     });
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
-
+      browser.fill("surname", "Vespucci").pressButton("submit", () => {
+        browser.assert.success();
+        browser.assert.text("span#name", "Amerigo");
+        browser.assert.text("span#surname", "Vespucci");
+        browser.assert.elements("span#dates", 1);
+        done();
+      });
       done();
     });
   });
